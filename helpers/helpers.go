16fdc62cde71f712ddc03fa8e0464e6fcde161ff
diraven <oleksa.pavlenko@gmail.com>
2017-04-27 23:00:58 +0300

    Helpers also golinted.

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -7,21 +7,25 @@ import (
 	"time"
 )
 
+// UserAsMention returns a string in a form of <@0000000000> to be included into discord message.
 func UserAsMention(u *discordgo.User) (s string) {
 	s = fmt.Sprintf("<@%s>", u.ID)
 	return
 }
 
+// ChannelAsMention returns a string in a form of <#0000000000> to be included into discord message.
 func ChannelAsMention(c *discordgo.Channel) (s string) {
 	s = fmt.Sprintf("<#%s>", c.ID)
 	return
 }
 
+// TimeToDiscordTimestamp returns time in a format that Discord API accepts.
 func TimeToDiscordTimestamp(t time.Time) (s string) {
 	s = t.Format(time.RFC3339)
 	return
 }
 
+// DiscordTimestampToTime returns time.Time parsed from discord API time string.
 func DiscordTimestampToTime(s string) (t time.Time, err error) {
 	t, err = time.Parse(time.RFC3339, s)
 	if err != nil {
@@ -30,12 +34,12 @@ func DiscordTimestampToTime(s string) (t time.Time, err error) {
 	return
 }
 
-func PointerToString(input_str string) (pointer_to_str *string) {
-	return &input_str
+// PointerToString returns pointer to the string given.
+func PointerToString(inputStr string) (pointerToStr *string) {
+	return &inputStr
 }
 
-// GetCreationTime is a helper function to get the time of creation of any ID.
-// ID: ID to get the time from
+// DiscordIDCreationTime gets the time of creation of any ID.
 func DiscordIDCreationTime(ID string) (t time.Time, err error) {
 	i, err := strconv.ParseInt(ID, 10, 64)
 	if err != nil {
@@ -46,12 +50,12 @@ func DiscordIDCreationTime(ID string) (t time.Time, err error) {
 	return
 }
 
+// BoolToInt converts bool:true to int:1 and bool:false to int:0.
 func BoolToInt(input bool) (output int) {
 	if input {
 		return 1
-	} else {
-		return 0
 	}
+	return 0
 }
 
 // ConsumeTerm removes and returns first term from the given string.
