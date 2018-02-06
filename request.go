773124e60e1d1b9a0a61981d113a8d54bf446bd4
Alex Pavlenko <oleksa.pavlenko@gmail.com>
2018-02-06 13:53:01 +0200

    request cleanup and comments.

--- request.go
+++ request.go
@@ -2,8 +2,7 @@ package sugo
 
 import (
 	"github.com/bwmarrin/discordgo"
-	"github.com/pkg/errors"
-	"fmt"
+	"errors"
 )
 
 // Request contains message context data along with some helpers to retrieve more information.
@@ -22,7 +21,7 @@ func (r *Request) GetGuild() (*discordgo.Guild, error) {
 	if r.Channel.GuildID != "" {
 		guild, err := r.Sugo.Session.State.Guild(r.Channel.GuildID)
 		if err != nil {
-			return nil, errors.Wrap(err, "getting guild failed")
+			return nil, errors.New("unable to get guild for request: " + r.Query)
 		}
 
 		return guild, nil
@@ -51,5 +50,5 @@ func (r *Request) IsChannelDM() bool {
 
 // WrapError error wraps error with additional request info.
 func (r *Request) WrapError(e error, text string) error {
-	return errors.Wrap(e, fmt.Sprintf("command error: %s: %s", r.Command.GetPath(), r.Query))
+	return errors.New("request error: " + r.Command.GetPath() + ": " + r.Query)
 }
