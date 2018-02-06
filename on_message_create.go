55a3414f7bd3b86cc7a52d65b12c6099adb5f939
Alex Pavlenko <oleksa.pavlenko@gmail.com>
2018-02-06 14:08:57 +0200

    Reducing function cyclomatic complexity.

--- on_message_create.go
+++ on_message_create.go
@@ -6,6 +6,43 @@ import (
 	"strings"
 )
 
+func isTriggered(req *Request) bool {
+	if req.Channel.Type == discordgo.ChannelTypeDM {
+		// It's Direct Messaging Channel. Every message here is in fact a direct message to the bot, so we consider
+		// it to be command without any further checks for prefixes.
+		return true
+	} else if req.Channel.Type == discordgo.ChannelTypeGuildText || req.Channel.Type == discordgo.ChannelTypeGroupDM {
+		// It's either Guild Text Channel or multiple people direct group Channel.
+		// In order to detect command we need to check for bot Triggereq.
+
+		// If bot Trigger is set and command starts with that Trigger:
+		if req.Sugo.Trigger != "" && strings.HasPrefix(req.Query, req.Sugo.Trigger) {
+			// Replace custom Trigger with bot mention for it to be detected as bot Triggereq.
+			req.Query = strings.Replace(req.Query, req.Sugo.Trigger, req.Sugo.Self.Mention(), 1)
+		}
+
+		// If bot nick was changed on the server - it will have ! in it's mention, so we need to remove that in order
+		// for mention detection to work right.
+		if strings.HasPrefix(req.Query, "<@!") {
+			req.Query = strings.Replace(req.Query, "<@!", "<@", 1)
+		}
+
+		// If the message starts with bot mention:
+		if strings.HasPrefix(strings.TrimSpace(req.Query), req.Sugo.Self.Mention()) {
+			// Remove bot Trigger from the string.
+			req.Query = strings.TrimSpace(strings.TrimPrefix(req.Query, req.Sugo.Self.Mention()))
+			// Bot is triggered.
+			return true
+		} else {
+			// Otherwise bot is not triggered.
+			return false
+		}
+	}
+
+	// We ignore all other channel types and consider bot not triggered.
+	return false
+}
+
 // onMessageCreate is a lowest level handler for bot. All the request building and command searching magic happen here.
 func (sg *Instance) onMessageCreate(m *discordgo.Message) {
 	var err error
@@ -16,90 +53,64 @@ func (sg *Instance) onMessageCreate(m *discordgo.Message) {
 	}
 
 	// Instantiate request.
-	var r = &Request{}
+	var req = &Request{}
 
 	// Put bot pointer into the appropriate request var for later reference.
-	r.Sugo = sg
+	req.Sugo = sg
 
 	// Put message into request.
-	r.Message = m
+	req.Message = m
 
 	// Put initial query into request.
-	r.Query = m.Content
+	req.Query = m.Content
 
 	// Get message channel and put it into the request.
-	r.Channel, err = sg.Session.State.Channel(r.Message.ChannelID)
+	req.Channel, err = sg.Session.State.Channel(req.Message.ChannelID)
 	if err != nil {
-		sg.HandleError(errors.New("unable to get channel: " + err.Error() + " (" + r.Query + ")"))
+		sg.HandleError(errors.New("unable to get channel: " + err.Error() + " (" + req.Query + ")"))
 	}
 
-	if r.Channel.Type == discordgo.ChannelTypeDM {
-		// It's Direct Messaging Channel. Every message here is in fact a direct message to the bot, so we consider
-		// it to be command without any further checks for prefixes.
-
-	} else if r.Channel.Type == discordgo.ChannelTypeGuildText || r.Channel.Type == discordgo.ChannelTypeGroupDM {
-		// It's either Guild Text Channel or multiple people direct group Channel.
-		// In order to detect command we need to check for bot Trigger.
-
-		// If bot Trigger is set and command starts with that Trigger:
-		if sg.Trigger != "" && strings.HasPrefix(r.Query, sg.Trigger) {
-			// Replace custom Trigger with bot mention for it to be detected as bot Trigger.
-			r.Query = strings.Replace(r.Query, sg.Trigger, sg.Self.Mention(), 1)
-		}
-
-		// If bot nick was changed on the server - it will have ! in it's mention, so we need to remove that in order
-		// for mention detection to work right.
-		if strings.HasPrefix(r.Query, "<@!") {
-			r.Query = strings.Replace(r.Query, "<@!", "<@", 1)
-		}
-
-		// If the message starts with bot mention:
-		if strings.HasPrefix(strings.TrimSpace(r.Query), sg.Self.Mention()) {
-			// Remove bot Trigger from the string.
-			r.Query = strings.TrimSpace(strings.TrimPrefix(r.Query, sg.Self.Mention()))
-		} else {
-			// Ignore the message otherwise and do nothing.
-			return
-		}
-
+	// Make sure bot is triggered by the request.
+	if !isTriggered(req) {
+		return
 	}
 
 	// Search for applicable command.
-	r.Command, err = sg.FindCommand(r, r.Query)
+	req.Command, err = sg.FindCommand(req, req.Query)
 	if err != nil {
-		sg.HandleError(errors.New("command search error: " + err.Error() + " (" + r.Query + ")"))
+		sg.HandleError(errors.New("command search error: " + err.Error() + " (" + req.Query + ")"))
 	}
 
 	// If we did not find matching command, try applying alias and search again.
-	if r.Command == nil {
+	if req.Command == nil {
 		// Apply aliases if any applicable.
 		for _, alias := range *sg.aliases {
-			if strings.HasPrefix(strings.TrimSpace(r.Query), alias.from) {
-				r.Query = strings.Replace(r.Query, alias.from, alias.to, 1)
+			if strings.HasPrefix(strings.TrimSpace(req.Query), alias.from) {
+				req.Query = strings.Replace(req.Query, alias.from, alias.to, 1)
 				break // we apply only one alias that matched first.
 			}
 		}
 
 		// Search for applicable command again after alias was applied.
-		r.Command, err = sg.FindCommand(r, r.Query)
+		req.Command, err = sg.FindCommand(req, req.Query)
 		if err != nil {
-			sg.HandleError(errors.New("command search error: " + err.Error() + " (" + r.Query + ")"))
+			sg.HandleError(errors.New("command search error: " + err.Error() + " (" + req.Query + ")"))
 		}
 	}
 
 	// If we have found applicable command:
-	if r.Command != nil {
+	if req.Command != nil {
 		// Remove command Trigger from message string.
-		r.Query = strings.TrimSpace(strings.TrimPrefix(r.Query, r.Command.GetPath()))
+		req.Query = strings.TrimSpace(strings.TrimPrefix(req.Query, req.Command.GetPath()))
 
 		// And execute command.
-		err = r.Command.execute(sg, r)
+		err = req.Command.execute(sg, req)
 		if err != nil {
 			if strings.Contains(err.Error(), "\"code\": 50013") {
 				// Insufficient permissions.
-				sg.HandleError(errors.New("permissions error: " + err.Error() + " (" + r.Query + ")"))
+				sg.HandleError(errors.New("permissions error: " + err.Error() + " (" + req.Query + ")"))
 			}
-			sg.HandleError(errors.New("command execution error: " + err.Error() + " (" + r.Query + ")"))
+			sg.HandleError(errors.New("command execution error: " + err.Error() + " (" + req.Query + ")"))
 		}
 	}
 
